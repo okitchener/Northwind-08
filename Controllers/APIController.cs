36e0a2ef21cc1a934ba2c20fe179c229ed70709d
Owen Kitchener <okitchener@my.wctc.edu>
2026-04-13 08:28:06 -0500

    [7.05] Return all products from a category filtered by discontinued = T/F

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -16,8 +16,11 @@ namespace Northwind.Controllers
         [HttpGet, Route("api/product/discontinued/{discontinued}")]
         // returns all products where discontinued = true/false
         public IEnumerable<Product> GetDiscontinued(bool discontinued) => _dataContext.Products.Where(p => p.Discontinued == discontinued).OrderBy(p => p.ProductName);
- [HttpGet, Route("api/category/{CategoryId}/product")]
+        [HttpGet, Route("api/category/{CategoryId}/product")]
         // returns all products in a specific category
         public IEnumerable<Product> GetByCategory(int CategoryId) => _dataContext.Products.Where(p => p.CategoryId == CategoryId).OrderBy(p => p.ProductName);
+    [HttpGet, Route("api/category/{CategoryId}/product/discontinued/{discontinued}")]
+        // returns all products in a specific category where discontinued = true/false
+        public IEnumerable<Product> GetByCategoryDiscontinued(int CategoryId, bool discontinued) => _dataContext.Products.Where(p => p.CategoryId == CategoryId && p.Discontinued == discontinued).OrderBy(p => p.ProductName);
     }
 }
