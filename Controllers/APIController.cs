2b6fb6a1219e32c1b70ec62340acfc079138757c
Owen Kitchener <okitchener@my.wctc.edu>
2026-04-13 08:24:56 -0500

    [7.02] Create API method to expose specific product

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -10,5 +10,8 @@ namespace Northwind.Controllers
         [HttpGet, Route("api/product")]
         // returns all products
         public IEnumerable<Product> Get() => _dataContext.Products.OrderBy(p => p.ProductName);
+        [HttpGet, Route("api/product/{id}")]
+        // returns specific product
+        public Product Get(int id) => _dataContext.Products.FirstOrDefault(p => p.ProductId == id);
     }
 }
