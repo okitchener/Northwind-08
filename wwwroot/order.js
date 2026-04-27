c9a96aa9d82d4bf6e35601b76b61901892b27fb2
Owen Kitchener <okitchener@my.wctc.edu>
2026-04-27 09:46:11 -0500

    red part 2 electric hulabaloo

--- wwwroot/order.js
+++ wwwroot/order.js
@@ -1,6 +1,6 @@
 // if orders index.html required date is later than today add the class of overdue to the tr element
 document.addEventListener("DOMContentLoaded", function() {   
-        const requiredDate = new Date(item.dataset['requireddate']);
+        const requiredDate = ???);
         const today = new Date();
         if (requiredDate < today) {
             item.classList.add("overdue");
